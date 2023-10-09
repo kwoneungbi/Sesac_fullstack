adedae758f43a2c7b440f5bd57b509f58ac4ec02
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-10-09 14:11:03 +0900

    purefun_week refectoring

--- Javascript/Try_This/purefn_week_name.js
+++ Javascript/Try_This/purefn_week_name.js
@@ -1,52 +1,34 @@
-// const weeks = ["일", "월", "화", "수", "목", "금", "토"];
-// let widx = -1;
-// const getNextWeek = () => {
-//   widx += 1; // side-effect!
-//   if (widx >= weeks.length) widx = 0;
-//   return `${weeks[widx]}요일`;
-// };
+const weeks = ["일", "월", "화", "수", "목", "금", "토"];
+let widx = -1;
+const getNextWeek = () => {
+  widx += 1; // side-effect!
+  if (widx >= weeks.length) widx = 0;
+  return `${weeks[widx]}요일`;
+};
 
-// let cnt = 0;
-// const intl = setInterval(() => {
-//   // widx += 2; // side-effect!
-//   console.log("call", cnt, getNextWeek());
-//   if ((cnt += 1) === 8) clearInterval(intl);
-// }, 1000);
+let cnt = 0;
+const intl = setInterval(() => {
+  // widx += 2; // side-effect!
+  console.log("call", cnt, getNextWeek());
+  if ((cnt += 1) === 8) clearInterval(intl);
+}, 100);
 
-// getNextWeek();
+getNextWeek();
 
 // 위의 코드를 순수 함수로짜보기 , 즉시 실행 함수를 이용한 클로저
 
-const weeks = ["일", "월", "화", "수", "목", "금", "토"];
+const weeks1 = ["일", "월", "화", "수", "목", "금", "토"];
 
 const getNextWeekMaker = (() => {
   let widx = -1;
   return function () {
     widx += 1;
-    if (widx >= weeks.length) widx = 0;
-    return `${weeks[widx]}요일`;
+    return `${weeks1[widx % 7]}요일`;
   };
 })();
 
-let cnt = 0;
-const intl = setInterval(() => {
-  console.log("call", cnt, getNextWeekMaker());
-  if ((cnt += 1) === 8) clearInterval(intl);
+let cnt1 = 0;
+const intl1 = setInterval(() => {
+  console.log("call", cnt1, getNextWeekMaker());
+  if ((cnt1 += 1) === 8) clearInterval(intl1);
 }, 100);
-
-// const getNextWeek1 = () => {
-//   let widx = -1;
-//   let cnt = 0;
-
-//   const getNextWeek = () => {
-//     widx += 1;
-//     if (widx >= weeks.length) widx = 0;
-//     return `${weeks[widx]}요일`;
-//   };
-
-//   const intl = setInterval(() => {
-//     console.log("call", cnt, getNextWeek());
-//     if ((cnt += 1) === 8) clearInterval(intl);
-//   }, 100);
-// };
-// getNextWeek1();
