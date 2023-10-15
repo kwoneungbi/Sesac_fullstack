d0d55c9a1a722bc56e0d8ce7ca8b9c86491d101e
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-10-15 16:56:52 +0900

    Making a non-pure function a pure function

---
+++ Javascript/Try_This/CreatePureFnc/push,pop,shift,unshift.js
@@ -0,0 +1,38 @@
+/*
+다음과 같은 push, pop, shift, unshift 를 순수 함수로 작성하시오.
+(단, 입력값은 다음 예시로 한정함)
+const arr = [1, 2, 3, 4];
+console.log(push(arr, 5, 6));  // [1, 2, 3, 4, 5, 6]
+console.log(pop(arr));         // 4
+console.log(pop(arr, 2));      // 2개 팝! ⇒ [3, 4]
+console.log(unshift(arr, 0));  // [0, 1, 2, 3, 4]
+console.log(unshift(arr, 7, 8));  // [7, 8, 1, 2, 3, 4]
+console.log(shift(arr));       // [2, 3, 4]
+console.log(shift(arr, 2));    // [3, 4]
+console.log(arr); // [1, 2, 3, 4] 
+*/
+const arr = [1, 2, 3, 4];
+
+const push = (array, ...args) => [...array, ...args];
+
+const pop = (array, a) =>
+  a === undefined ? array[array.length - 1] : array.slice(-a);
+
+const unshift = (array, ...args) => {
+  const copyArray = [...array];
+  args.length === 1 ? copyArray.unshift(args[0]) : copyArray.unshift(...args);
+  return copyArray;
+};
+
+const shift = (array, a) => {
+  return a === undefined ? array.slice(1) : array.slice(a);
+};
+
+console.log(push(arr, 5, 6)); // [1, 2, 3, 4, 5, 6]
+console.log(pop(arr)); // 4
+console.log(pop(arr, 2)); // 2개 팝! ⇒ [3, 4]
+console.log(unshift(arr, 0)); // [0, 1, 2, 3, 4]
+console.log(unshift(arr, 7, 8)); // [7, 8, 1, 2, 3, 4]
+console.log(shift(arr)); // [2, 3, 4]
+console.log(shift(arr, 2)); // [3, 4]
+console.log(arr); // [1, 2, 3, 4]
