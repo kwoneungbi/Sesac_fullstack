54d338cae575f3ef36faaa451ce49a53fe5d4e3e
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-10-30 12:55:30 +0900

    queue class refectoring

--- Javascript/Try_This/Object Oriented Programming/queue.js
+++ Javascript/Try_This/Object Oriented Programming/queue.js
@@ -22,3 +22,30 @@ queue.enqueue(3); // 추가하기
 assert.deepStrictEqual(queue.toArray(), [3]);
 queue.dequeue();
 assert.deepStrictEqual(queue.toArray(), []);
+
+// ----------------------------------------------------------------
+
+class Queue1 {
+  #arr;
+  constructor(...args) {
+    this.#arr = args;
+  }
+
+  enqueue(value) {
+    return this.#arr.push(value);
+  }
+
+  dequeue() {
+    return this.#arr.unshift();
+  }
+
+  toArray() {
+    return this.#arr;
+  }
+}
+
+const queue1 = new Queue();
+queue1.enqueue(3); // 추가하기
+assert.deepStrictEqual(queue1.toArray(), [3]);
+queue1.dequeue();
+assert.deepStrictEqual(queue1.toArray(), []);
