25a391ab1b961714fafee12e17d21f581720abc7
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-11-22 00:35:01 +0900

    Try This keypair

--- Javascript/Class/assertTest.js
+++
@@ -1,6 +0,0 @@
-var assert = require("assert");
-var x = { a: { n: 0 } };
-var y = { a: { n: 0 } };
-var z = { a: { n: "0" } };
-assert.deepStrictEqual(x, y); //OK
-assert.deepStrictEqual(x, z);
--- Javascript/Class/recursive.js
+++
@@ -1,8 +0,0 @@
-/* 재귀 함수를 사용하여 aguments n을 받아 n보다 작은 양의 정수를 다 더한 값을 출력하시오.*/
-
-const sum1 = (n) => {
-  if (n === 1) return 1;
-  return n + sum1(n - 1);
-};
-
-console.log(sum1(100));
