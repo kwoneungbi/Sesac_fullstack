b007ca4b3bed04c53a47a51a429d6652defadaeb
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-11-24 00:29:59 +0900

    Try this memoized

--- Javascript/Study/#5_Memoized/TryThis/memoizedFibonacci.js
+++ Javascript/Study/#5_Memoized/TryThis/memoizedFibonacci.js
@@ -1,90 +1,16 @@
-// 1. loop
-// 2. recursive function
-// 3. memoized function
+import assert from "assert";
 
-function fibonacci(n) {
-  const arr = [0, 1];
-  if (n <= 1) return n;
-  for (let i = 2; i <= n; i++) arr[i] = arr[i - 2] + arr[i - 1];
+const fibonacci = (() => {
+  const memo = [0, 1];
 
-  return arr[n];
-}
-
-console.time("LOOP");
-console.log(fibonacci(3));
-console.log(fibonacci(15));
-console.log(fibonacci(5000));
-console.timeEnd("LOOP");
-console.log("------------------");
-
-let rcnt = 0;
-function fibonacciByRecursive(n) {
-  rcnt += 1;
-  if (n <= 1) return n;
-  return fibonacciByRecursive(n - 2) + fibonacciByRecursive(n - 1);
-}
-
-// console.time('RECUR');
-// console.log(fiboR(3));
-// console.log(fiboR(15));
-// console.log(fiboR(30));
-// console.timeEnd('RECUR');
-// console.log('------------------', rcnt);
-
-let mcnt = 0;
-const memoizedFibonacci = memoized(function (n) {
-  mcnt += 1;
-  // console.log('🚀  mcnt:', mcnt);
-  if (n <= 1) return n;
-  return memoizedFibonacci(n - 2) + memoizedFibonacci(n - 1);
-});
-
-console.time("MemoFIBO");
-console.log(memoizedFibonacci(3));
-console.log(memoizedFibonacci(15));
-// console.log(memoizedFibonacci(10000));
-console.log("------------------");
-
-function memoized(fn) {
-  const memoizedTable = {};
-  return function (k) {
-    return memoizedTable[k] || (memoizedTable[k] = fn(k));
-  };
-}
-
-console.log("=============================");
-
-function fiboArray(n) {
-  const arr = [0, 1];
-  if (n <= 1) return n;
-  for (let i = 2; i <= n; i++) arr[i] = arr[i - 2] + arr[i - 1];
-
-  const rets = [];
-  for (let i = 0; i <= n; i += 1) rets[i] = arr[i];
-  return rets;
-}
-console.log(fiboArray(3));
-console.log(fiboArray(5));
-
-console.log("=============================");
-function fiboCachedArray() {
-  const cachedArray = [0, 1]; // [0, 1, 1, 2, 3, 5]
-
-  return function (n) {
-    if (cachedArray.length <= n) {
-      for (let i = cachedArray.length; i <= n; i++)
-        cachedArray[i] = cachedArray[i - 2] + cachedArray[i - 1];
+  return (n) => {
+    if (memo[n]) return memo[n];
+    for (let i = memo.length; i <= n; i += 1) {
+      memo[i] = memo[i - 2] + memo[i - 1];
     }
-
-    const rets = [];
-    for (let i = 0; i <= n; i += 1) rets[i] = cachedArray[i];
-    return rets;
-    // return cachedArray.slice(0, n + 1);
+    return memo[n];
   };
-}
+})();
 
-const fibonacciX = fiboCachedArray();
-console.log(fibonacciX(3));
-console.log(fibonacciX(5));
-console.log(fibonacciX(7));
-console.log(fibonacciX(2));
+assert.deepEqual(fibonacci(5), 5);
+assert.deepEqual(fibonacci(7), 13);
