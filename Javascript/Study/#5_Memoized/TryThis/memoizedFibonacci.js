a96026590f4a69985436d7ba5fab2f81177d70cf
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-11-24 17:43:04 +0900

    Make it recursive

--- Javascript/Study/#5_Memoized/TryThis/memoizedFibonacci.js
+++ Javascript/Study/#5_Memoized/TryThis/memoizedFibonacci.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 
+// 1. memoize
 const fibonacci = (() => {
   const memo = [0, 1];
 
@@ -14,3 +15,22 @@ const fibonacci = (() => {
 
 assert.deepEqual(fibonacci(5), 5);
 assert.deepEqual(fibonacci(7), 13);
+
+// 2. memoize Recursive
+const memoizedFibo = (fn) => {
+  const memo = [1];
+
+  return (k) => {
+    return memo[k - 1] || (memo[k - 1] = fn(k));
+  };
+};
+
+const fibonacciRecur = memoizedFibo((n) => {
+  if (n <= 1) return n;
+  return fibonacciRecur(n - 1) + fibonacciRecur(n - 2);
+});
+
+assert.deepEqual(fibonacciRecur(5), 5);
+[1, 1, 2, 3, 5];
+assert.deepEqual(fibonacciRecur(7), 13);
+[1, 1, 2, 3, 5, 8, 13];
