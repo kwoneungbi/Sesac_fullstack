349c246581283e229fecb54cdc689aee7638f90b
kwoneungbi <rnjsdmsql7290@gmail.com>
2023-11-27 21:58:26 +0900

    UtilityType Parameters

---
+++ Typescript/Study/trythis_UtilityType/parameters.ts
@@ -0,0 +1,24 @@
+/*
+// regist 함수가 다음과 같을 때 파라미터 처리를 해보세요.
+function registUserObj({ name, age }: { name: string; age: number }) {
+  const id = 100;
+  return { id, name, age };
+}
+
+type RegistUserObj = <이 부분을 작성해 보세요>;
+
+const paramObj: RegistUserObj = { name: 'Hong', age: 32 };
+const newUser2 = registUserObj(paramObj);
+console.log('newUser2:', newUser2);
+*/
+
+function registUserObj({ name, age }: { name: string; age: number }) {
+  const id = 100;
+  return { id, name, age };
+}
+
+type RegistUserObj = Parameters<typeof registUserObj>[0];
+
+const paramObj: RegistUserObj = { name: "Hong", age: 32 };
+const newUser2 = registUserObj(paramObj);
+console.log("newUser2:", newUser2);
